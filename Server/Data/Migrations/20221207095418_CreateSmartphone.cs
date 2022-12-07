796d604b6c8d17b6131c0eca7f53a56825fc52d7
Th3shoterZ <46137910+Th3shoterZ@users.noreply.github.com>
2022-12-07 14:55:44 +0100

    [wip] updated migration

--- Server/Data/Migrations/20221207095418_CreateSmartphone.cs
+++ Server/Data/Migrations/20221207095418_CreateSmartphone.cs
@@ -17,8 +17,92 @@ namespace SmartphonePortalVervoortWagner.Server.Data.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false),
                     Name = table.Column<string>(type: "nvarchar(128)", nullable: false),
-                    Category = table.Column<string>(type: "nvarchar(128)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(128)", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Smartphone", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(400)", nullable: false),
+                    ManufacturorInfo = table.Column<string>(type: "nvarchar(400)", nullable: false),
+                    DisplaySize = table.Column<string>(type: "nvarchar(64)", nullable: false),
+                    Resolution = table.Column<string>(type: "nvarchar(64)", nullable: false),
+                    Weight = table.Column<string>(type: "nvarchar(64)", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneDetails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+               name: "Picture",
+               columns: table => new
+               {
+                   Id = table.Column<int>(type: "int", nullable: false),
+                   Title = table.Column<string>(type: "nvarchar(128)", nullable: false),
+                   RelativePath = table.Column<string>(type: "nvarchar(128)", nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Picture", x => x.Id);
+               });
+
+            migrationBuilder.CreateTable(
+               name: "Processor",
+               columns: table => new
+               {
+                   Id = table.Column<int>(type: "int", nullable: false),
+                   Title = table.Column<string>(type: "nvarchar(64)", nullable: false),
+                   CoreCount = table.Column<string>(type: "nvarchar(64)", nullable: false),
+                   Gigaherz = table.Column<string>(type: "nvarchar(64)", nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Processor", x => x.Id);
+               });
+
+            migrationBuilder.CreateTable(
+               name: "Review",
+               columns: table => new
+               {
+                   Id = table.Column<int>(type: "int", nullable: false),
+                   Title = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                   Text = table.Column<string>(type: "nvarchar(1000)", nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Review", x => x.Id);
+               });
+
+            migrationBuilder.CreateTable(
+               name: "Comment",
+               columns: table => new
+               {
+                   Id = table.Column<int>(type: "int", nullable: false),
+                   Text = table.Column<string>(type: "nvarchar(1000)", nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Comment", x => x.Id);
+               });
+
+            migrationBuilder.CreateTable(
+               name: "Rating",
+               columns: table => new
+               {
+                   Id = table.Column<int>(type: "int", nullable: false),
+                   Stars = table.Column<int>(type: "int", nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Rating", x => x.Id);
+               });
         }
     }
 }
