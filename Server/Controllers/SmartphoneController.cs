ef2bfcdae636945bfcfca7ff83ef9a41d5cf75a9
Th3shoterZ <46137910+Th3shoterZ@users.noreply.github.com>
2023-03-29 21:10:04 +0200

    [wip] controllers

---
+++ Server/Controllers/SmartphoneController.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartphonePortal_Vervoort_Wagner.Server.Controllers;
+
+public class SmartphoneController : ControllerBase
+{
+}
---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartphonePortal_Vervoort_Wagner.Server.Data;
+using SmartphonePortal_Vervoort_Wagner.Server.Models;
+
+namespace SmartphonePortal_Vervoort_Wagner.Server.Controllers;
+
+public class UserController : ControllerBase
+{
+    private Applicato
+    public UserController(ApplicationDbContext dbContext)
+    {
+        
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetProfile(int profileId)
+    {
+        
+        return Ok(new ApplicationUser());
+    }
+}
