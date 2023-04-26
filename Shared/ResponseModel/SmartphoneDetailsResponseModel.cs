0345ba562770d31a4a511d8ad34fc2ce2eea5486
Th3shoterZ <46137910+Th3shoterZ@users.noreply.github.com>
2023-04-26 14:54:56 +0200

    [wip] optimize api calls

---
+++ Shared/ResponseModel/SmartphoneDetailsResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using SmartphonePortal_Vervoort_Wagner.Shared.ViewModels;
+
+namespace SmartphonePortal_Vervoort_Wagner.Shared.ResponseModel;
+
+public class SmartphoneDetailsResponseModel
+{
+    public SmartphoneViewModel Smartphone { get; set; } = new();
+    public List<ReviewViewModel> Reviews { get; set; } = new List<ReviewViewModel>();
+    public ReviewViewModel? UserReview { get; set; }
+}
