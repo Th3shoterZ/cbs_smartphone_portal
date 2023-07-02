e1484c2ccd131cb56fc7dcc227c855ec96b43f00
Th3shoterZ <wout.vervoort.99@gmail.com>
2023-07-02 20:44:36 +0200

    [wip] adding picture logic

--- Shared/Requests/PictureCreationRequest.cs
+++ Shared/Requests/PictureCreationRequest.cs
@@ -1,5 +1,11 @@
-﻿namespace SmartphonePortal_Vervoort_Wagner.Shared.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartphonePortal_Vervoort_Wagner.Shared.Requests;
 
 public class PictureCreationRequest
 {
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    [Required]
+    public int SmartphoneId { get; set; }
 }
