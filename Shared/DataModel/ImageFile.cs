14b0ca397ba562863ec3feeec6fca0c443d66c8c
Th3shoterZ <wout.vervoort.99@gmail.com>
2023-07-02 20:13:41 +0200

    [wip] adding picture support

---
+++ Shared/DataModel/ImageFile.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartphonePortal_Vervoort_Wagner.Shared.DataModel;
+
+public class ImageFile
+{
+
+    public string FileName { get; set; } = string.Empty;
+}
---
+++ Shared/Responses/FileUploadResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartphonePortal_Vervoort_Wagner.Shared.Responses;
+
+public class FileUploadResponse
+{
+    public bool Uploaded { get; set; }
+    public string? FileName { get; set; }
+    public string? StoredFileName { get; set; }
+    public int ErrorCode { get; set; }
+}
